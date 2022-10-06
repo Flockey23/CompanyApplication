e7b07fb253dab2b527caf142990fac8b340e7fc7
Alexey <flockey23@mail.ru>
2022-10-07 01:53:39 +0500

    Рефакторинг home экрана, вынес функции в отдельный класс

---
+++ lib/presentation/functions/delete_companies.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:myapp/domain/state/home/home_state.dart';
+import 'package:myapp/presentation/pages/Company/home.dart';
+
+class DeleteCompanies {
+  showDeleteDialog(HomeState homeState, BuildContext context) {
+    if (homeState.deleteCompanies.isNotEmpty) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text("Удаление"),
+              content: const Text(
+                  "Вы уверены что хотите удалить выделенные компании?"),
+              actions: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("Отмена")),
+                ElevatedButton(
+                    onPressed: (){
+                      deleteSelectedCompanies(homeState, context);
+            },
+            child: const Text("Подтвердить")),
+              ],
+            );
+          });
+    } else {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+                title: const Text("Ошибка!"),
+                content: const Text("Выберите компании для удаления"),
+                actions: [
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("Продолжить"))
+                ]);
+          });
+    }
+  }
+
+  deleteSelectedCompanies(HomeState homeState, BuildContext context) async {
+    await homeState.deleteSelectedCompanies();
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const Home()));
+  }
+}
--- lib/presentation/pages/Company/home.dart
+++ lib/presentation/pages/Company/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/presentation/functions/delete_companies.dart';
 import 'package:myapp/presentation/pages/Authorization/authorization.dart';
 import 'package:myapp/domain/state/home/home_state.dart';
 import 'package:myapp/internal/dependencies/home_module.dart';
@@ -23,78 +24,41 @@ class _HomeState extends State<Home> {
     getCompany();
   }
 
+  final DeleteCompanies deleteCompanies = DeleteCompanies();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Company Data'),
-        toolbarHeight: MediaQuery.of(context).size.height/10,
+        toolbarHeight: MediaQuery.of(context).size.height / 10,
         leading: IconButton(
             icon: const Icon(Icons.logout),
             onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Authorization()));
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const Authorization()));
             }),
         actions: [
           IconButton(
-              onPressed: showDeleteDialog,
+              onPressed: () {
+                deleteCompanies.showDeleteDialog(_homeState, context);
+              },
               icon: const Icon(Icons.delete)),
         ],
       ),
-      floatingActionButton: FloatingActionButton(child: const Icon(Icons.add_circle),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add_circle),
           onPressed: () {
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => const CompanyAdd()));
-      }),
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const CompanyAdd()));
+          }),
       body: CompanyInformation(homeState: _homeState),
     );
   }
 
   getCompany() async {
     await _homeState.getAllCompanies();
-  }
-
-  showDeleteDialog() {
-    if (_homeState.deleteCompanies.isNotEmpty) {
-      showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text("Удаление"),
-              content: const Text(
-                  "Вы уверены что хотите удалить выделенные компании?"),
-              actions: [
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text("Отмена")),
-                ElevatedButton(
-                    onPressed: deleteSelectedCompanies,
-                    child: const Text("Подтвердить")),
-              ],
-            );
-          });
-    } else {
-      showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-                title: const Text("Ошибка!"),
-                content: const Text("Выберите компании для удаления"),
-                actions: [
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text("Продолжить"))
-                ]);
-          });
-    }
-  }
-
-  deleteSelectedCompanies() async {
-    await _homeState.deleteSelectedCompanies();
-    Navigator.push(context, MaterialPageRoute(builder: (context) => const Home()));
   }
 }
